463e898de5e88e2db9d53d437832934f6adb2a50
Yehonatan z <yehonatan.ziv@gmail.com>
2022-01-01 17:01:10 +0200

    connect to db - first

---
+++ demo/src/main/java/com.project.animalproject91455/AnimalProject91455Application.java
@@ -0,0 +1,13 @@
+package com.project.animalproject91455;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalProject91455Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalProject91455Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/animalproject91455/AnimalProject91455ApplicationTests.java
@@ -0,0 +1,13 @@
+package animalproject91455;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalProject91455ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
