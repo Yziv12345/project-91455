eac15174070d9bc08a53b6e2a0df9c10f53bc89f
Yehonatan z <yehonatan.ziv@gmail.com>
2022-01-15 20:13:03 +0200

    rename users repository , add actions entity

--- demo/src/main/java/com.project.animalproject91455/repository/UsersRepository.java
+++ demo/src/main/java/com.project.animalproject91455/repository/UsersRepository.java
@@ -3,6 +3,6 @@ package com.project.animalproject91455.repository;
 import com.project.animalproject91455.interfaces.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CustomersRepository extends MongoRepository<Users, Integer>{
+public interface UsersRepository extends MongoRepository<Users, Integer>{
 }
 
